dff36f3b790769c4eac5cab9e9226e07845b9a60
cwest9400 <chrstphrwest@gmail.com>
2023-03-19 14:09:16 -0400

    creating buy function

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 let cash = 100
-let wood = {
-    quantity: 10,
-    price: 2.00,
-}
 
-let nail = {
-    quantity: 75,
-    price: .20,
-}
+let materials = [
+    wood = {
+        quantity: 10,
+        price: 2.00,
+    },
+    nail = {
+        quantity: 75,
+        price: .20,
+    },
 
-function buy(material, qty) {
-    console.log(`previous ${material} quantity: ${material.quantiy}`)
+];
+
+
+function buy(material, num) {
+    console.log(`previous ${material} quantity: ${material.num}`)
     if(!material) {
         console.log("unknown material")
     }
     cash = cash - material.price
-    material = material.quantity + qty
-    console.log
+    material = material.quantity + num
+    console.log(`new ${material} quantity: ${material.num}`)
 }
+
+buy("nail",10)
