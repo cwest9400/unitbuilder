f949c762ea86eabd8b1c88c33af96f40f2e28c35
cwest9400 <chrstphrwest@gmail.com>
2023-03-19 13:54:21 -0400

    creating buy function

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("sanity check")
+let cash = 100
+let wood = {
+    quantity: 10,
+    price: 2.00,
+}
+
+let nail = {
+    quantity: 75,
+    price: .20,
+}
+
+function buy(material, qty) {
+    console.log(`previous ${material} quantity: ${material.quantiy}`)
+    if(!material) {
+        console.log("unknown material")
+    }
+    cash = cash - material.price
+    material = material.quantity + qty
+    console.log
+}
