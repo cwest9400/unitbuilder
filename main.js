38a6c9fad1150edac8cf60d572a23f086687881d
cwest9400 <chrstphrwest@gmail.com>
2023-03-19 14:30:00 -0400

    error handling for buy function

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let cash = 100
+let cash = 2
 
 let materials = [
     wood = {
+        name: "wood",
         quantity: 10,
         price: 2.00,
     },
     nail = {
+        name: "nail",
         quantity: 75,
         price: .20,
     },
@@ -13,14 +15,22 @@ let materials = [
 ];
 
 
-function buy(material, num) {
-    console.log(`previous ${material} quantity: ${material.num}`)
+function buy(materialIndex, num) {
+    let material = materials[materialIndex];
     if(!material) {
         console.log("unknown material")
+        return
+    } else if(material.price * num >= 0) {
+        console.log("not enough cash")
+        return
     }
-    cash = cash - material.price
-    material = material.quantity + num
-    console.log(`new ${material} quantity: ${material.num}`)
+    console.log(`previous ${material.name} qty: ${material.quantity}`)
+    cash = cash - material.price * num;
+    material.quantity = material.quantity + num
+    console.log(`new ${material.name} qty: ${material.quantity}`)
+    
+    
 }
-
-buy("nail",10)
+console.log(`cash: $${cash}`)
+buy(0,10)
+console.log(`cash: $${cash}`)
