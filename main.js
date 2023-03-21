a9d2aad417d7bfa123bc8a2def14f6fe762bb519
cwest9400 <chrstphrwest@gmail.com>
2023-03-20 20:32:44 -0400

    added bom array of products

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
-
-
-
-
 let cash = 100
 
 let materials = [
     {
-      name: "wood",
-      quantity: 10,
-      price: 2.00,
-      uom: "pieces",
+        name: "wood",
+        quantity: 10,
+        price: 2.00,
+        uom: "pieces",
     },
     {
-      name: "nail",
-      quantity: 75,
-      price: 0.20,
-      uom: "pieces",
-    }
-  ];
+        name: "nail",
+        quantity: 75,
+        price: 0.20,
+        uom: "pieces",
+    },
+    {
+        name: "bracket",
+        quantity: 4,
+        price: 0.45,
+        uom: "pieces",
+    },
+    ,
+    {
+        name: "screw",
+        quantity: 20,
+        price: 0.37,
+        uom: "pieces",
+    },
+];
+
+let bom = [
+    {
+        name: "coaster set",
+        materials: [
+            {
+                name: "wood",
+                qty: 1,
+            },
+        ]
+    },
+    {
+        name: "coffee table",
+        materials: [
+            {
+                name: "wood",
+                qty: 5,
+            },
+            {
+                name: "nail",
+                qty: 20,
+            },
+            {
+                name: "screw",
+                qty: 12,
+            },
+        ]
+    },
+]
 
 function buy(materialIndex, num) {
     let material = materials[materialIndex];
-    if(!material) {
+    if (!material) {
         console.log("unknown material")
         return
-    } else if(material.price * num >= cash) {
-        console.log(`not enough cash to buy ${num} ${material.name}s. You only have ${cash} and you need ${material.price * num}`)
+    } else if (material.price * num > cash) {
+        console.log(`not enough cash to buy ${num} ${material.uom} of ${material.name}. You only have ${cash} and you need ${material.price * num}`)
         return
     }
     console.log(`previous ${material.name} qty: ${material.quantity}`)
@@ -34,6 +72,8 @@ function buy(materialIndex, num) {
     console.log(`new ${material.name} qty: ${material.quantity}`)
 }
 console.log(`cash: $${cash}`)
-buy(0,100)
+buy(0, 50)
 console.log(`cash: $${cash}`)
+
+
 
